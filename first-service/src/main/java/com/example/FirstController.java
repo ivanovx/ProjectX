78254d716297f5d5b7a48bf2cbb93da16e73bf50
Ivan Ivanov <me@ivanov.pro>
2023-04-03 01:43:06 +0300

    fix the parent project and add cassandra

--- first-service/src/main/java/com/example/FirstController.java
+++ first-service/src/main/java/com/example/FirstController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class FirstController {
     @RequestMapping("/first")
     public String greeting() {
         return "Hello from first!";
+    }
+
+    @RequestMapping("/first/{id}")
+    public String hello(@PathVariable String id) {
+        return "Hello from first with %s".formatted(id);
     }
 }
