92b5ba2f1e24e9ec83f2778ecbaf78f7f5ba5bad
Ivan Ivanov <me@ivanov.pro>
2023-05-05 12:43:59 +0300

    update Layout navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import Layout from './components/Layout';
 import SignIn from './views/SignIn';
+import Home from './views/Home';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
         children: [
+            { index: true, element: <Navigate to="home" /> },
+            { path: "home", element: <Home /> },
             { path: "map", element: <div>Map</div> },
+            { path: "dashboard", element: <div>Dashboard</div> },
             { path: "user/signin", element: <SignIn /> }
         ]
     }
--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,21 +1,10 @@
 import { Tabs, Tab } from '@mui/material';
 import React from 'react';
-import { Link, Outlet, useNavigate } from 'react-router-dom';
-import UserService from '../../modules/user-service';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 export default function Layout() {
-
-
     return (
         <div className="layout">
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/map">Map</Link>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/auth/signin">Sign In</Link>
-                <Link to="/auth/signup">Sign Up</Link>
-                <Link to="/auth/signout">Sign Out</Link>
-            </nav>
             <Navbar />
             <Outlet />
         </div>
@@ -23,30 +12,23 @@ export default function Layout() {
 }
 
 function Navbar() {
+    const location = useLocation();
     const navigate = useNavigate();
-    const [value, setValue] = React.useState<string | null>(null);
 
-    const handleChange = (event: React.SyntheticEvent, newValue:string) => {
-        setValue(oldValue => newValue);
-        navigate(newValue);
-    };
+    const onSignOut = (e: React.SyntheticEvent) => {
+        e.preventDefault();
 
-    
+        alert("Are you sure?");
+    }
+
     return (
-        <Tabs
-            value={value}
-            onChange={handleChange}
-            variant="scrollable"
-            scrollButtons={false}
-            aria-label="scrollable prevent tabs example"
-        >
-            <Tab value="home" label="Home" />
-            <Tab label="Item Two" />
-            <Tab label="Item Three" />
-            <Tab label="Item Four" />
-            <Tab label="Item Five" />
-            <Tab label="Item Six" />
-            <Tab label="Item Seven" />
+        <Tabs value={location.pathname} onChange={(e: React.SyntheticEvent, newValue: string) => navigate(newValue)} variant="fullWidth">
+            <Tab value="/home" label="Home" />
+            <Tab value="/map" label="Map" />
+            <Tab value="/dashboard" label="Dashboard" />
+            <Tab value="/user/signin" label="Sign In" />
+            <Tab value="/user/signup" label="Sign Up" />
+            <Tab onClick={onSignOut} label="Sign Out" />
         </Tabs>
     );
 }
