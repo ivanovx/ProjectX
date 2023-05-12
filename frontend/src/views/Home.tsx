77c9fd87c354df254b3101968d4f98d002064f4b
Ivan Ivanov <ivancivanovbg@gmail.com>
2023-05-12 10:53:46 +0300

    configure views

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -5,17 +5,16 @@ import DeviceService from "../modules/device-service";
 
 export default function Home() {
     const [devices, setDevices] = React.useState<any[]>([]);
-
+/*
     React.useEffect(() => {
         DeviceService
             .getAllDevices()
             .then(allDevices => setDevices(allDevices))
             .catch(console.log);
     }, []);
+    */
 
     return (
-        <Container>
-            {devices.map(device => <h1>{device.title}</h1>)}
-        </Container>
+        <Container>{devices.map(device => <h1>{device.title}</h1>)}</Container>
     );
 }
