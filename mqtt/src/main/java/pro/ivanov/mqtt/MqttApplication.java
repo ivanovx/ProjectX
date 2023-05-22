b7b1fbc3c6ab8b6af211cccc74974a6641ce9c0d
Ivan Ivanov <me@ivanov.pro>
2023-05-22 21:52:50 +0300

    base updates

--- mqtt/src/main/java/pro/ivanov/mqtt/MqttApplication.java
+++ mqtt/src/main/java/pro/ivanov/mqtt/MqttApplication.java
@@ -1,14 +1,19 @@
 package pro.ivanov.mqtt;
 
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
@@ -26,23 +31,35 @@ public class MqttApplication {
     }
 
     @Bean
-    public MessageChannel mqttInputChannel() {
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        //factory.setServerURIs();
+
+        MqttConnectOptions options = new MqttConnectOptions();
+                options.setServerURIs(new String[] {"tcp://localhost:1883"});
+
+        factory.setConnectionOptions(options);
+
+        return factory;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttOutboundChannel")
+    public MessageHandler mqttOutbound() {
+        MqttPahoMessageHandler messageHandler =
+                new MqttPahoMessageHandler("testClient", mqttClientFactory());
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultTopic("testTopic");
+        return messageHandler;
+    }
+
+    @Bean
+    public MessageChannel mqttOutboundChannel() {
         return new DirectChannel();
     }
 
-    @Bean
-    public MessageProducer inbound() {
-        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("tcp://localhost:1883", "testClient", "topic1", "topic2");
-        adapter.setCompletionTimeout(5000);
-        adapter.setConverter(new DefaultPahoMessageConverter());
-        adapter.setQos(1);
-        adapter.setOutputChannel(mqttInputChannel());
-        return adapter;
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "mqttChannel")
-    public MessageHandler handler() {
-        return message -> System.out.println(message.getPayload());
+    @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
+    public interface MyGateway {
+        void sendToMqtt(String data);
     }
 }
