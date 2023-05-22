9b2c8816fbca9718d9fb0249e0676fb4a3fa9a2f
Ivan Ivanov <me@ivanov.pro>
2023-05-22 22:55:14 +0300

    use session storage

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -2,28 +2,21 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container, Button, Grid } from '@mui/material';
 
-import AuthProvider, { useAuth } from '../Auth';
+import AuthProvider from '../Auth';
 
 export default function Layout() {
-    const auth = useAuth();
-
     return (
         <AuthProvider>
             <Container>
                 <Grid container spacing={2} margin="1rem auto 2rem">
                     <Grid item xs={6} md={8}>
                         <Button href="/" variant="text">Home</Button>
-                        {auth.token && <Button href="/dashboard" variant="text">Dashboard</Button>}
+                        <Button href="/dashboard" variant="text">Dashboard</Button>
                     </Grid>
                     <Grid item xs={6} md={4}>
-                        {!auth.token && (
-                            <>
-                                <Button href="/user/signup" variant="text">Sign Up</Button>
-                                <Button href="/user/signin" variant="text">Sign In</Button>
-                            </>
-                        )}
-
-                        {auth.token && <Button variant="text">Sign Out</Button>}
+                        <Button href="/user/signup" variant="text">Sign Up</Button>
+                        <Button href="/user/signin" variant="text">Sign In</Button>
+                        <Button variant="text">Sign Out</Button>
                     </Grid>
                 </Grid>
                 <Outlet />
--- frontend/src/modules/storage.ts
+++ frontend/src/modules/storage.ts
@@ -1,9 +1,9 @@
 export default class Storage {
     static get(key: string) {
-        return JSON.parse(window.localStorage.getItem(key)!);
+        return JSON.parse(window.sessionStorage.getItem(key)!);
     }
 
     static set(key: string, value: any) {
-        window.localStorage.setItem(key, JSON.stringify(value));
+        window.sessionStorage.setItem(key, JSON.stringify(value));
     }
 }
