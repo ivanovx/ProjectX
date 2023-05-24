ee6f5cc542363ed5af5a3a81ead76966fc02146c
Ivan Ivanov <me@ivanov.pro>
2023-05-24 14:10:03 +0300

    refactor dirs

--- backend/src/main/java/pro/ivanov/webapp/ApiException.java
+++
@@ -1,7 +0,0 @@
-package pro.ivanov.webapp;
-
-import java.time.LocalDateTime;
-
-import org.springframework.http.HttpStatus;
-
-public record ApiException(String message, HttpStatus httpStatus, LocalDateTime timestamp) { }
