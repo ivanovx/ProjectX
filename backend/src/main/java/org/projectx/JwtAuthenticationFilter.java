cad3dba8965271386eadc6b9fe1f3a615324b3ad
Ivan Ivanov <me@ivanov.pro>
2023-07-12 21:02:42 +0300

    using docker compose for resources

--- backend/src/main/java/org/projectx/JwtAuthenticationFilter.java
+++ backend/src/main/java/org/projectx/JwtAuthenticationFilter.java
@@ -27,11 +27,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().contains("/auth")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         final String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
