6a17e6f0f8db27646b545093fa195771e9edea4f
Ivan Ivanov <me@ivanov.pro>
2023-07-19 18:22:24 +0300

    create auth server

--- auth-server/src/main/java/org/projectx/auth/AuthApplication.java
+++ auth-server/src/main/java/org/projectx/auth/AuthApplication.java
@@ -1,4 +1,4 @@
-package org.ivanovx.projectx.auth;
+package org.projectx.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
