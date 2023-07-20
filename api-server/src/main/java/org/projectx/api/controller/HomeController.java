a5ad3ac2fb13c4994e8bd56416b6b3d48b169602
Ivan Ivanov <me@ivanov.pro>
2023-07-20 19:46:41 +0300

    update controllers

--- api-server/src/main/java/org/projectx/api/controller/HomeController.java
+++ api-server/src/main/java/org/projectx/api/controller/HomeController.java
@@ -1,8 +1,7 @@
 package org.projectx.api.controller;
 
-import org.projectx.api.response.DeviceResponse;
+import org.projectx.api.model.Device;
 import org.projectx.api.service.DeviceService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +19,7 @@ public class HomeController {
     }
 
     @GetMapping
-    public ResponseEntity<List<DeviceResponse>> index() {
+    public List<Device> index() {
         return this.deviceService.getAllActivated();
     }
 }
