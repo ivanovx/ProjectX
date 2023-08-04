8df4a56a6b03e7abefec8e29c88ea7be1f7bce6a
Ivan Ivanov <me@ivanov.pro>
2023-08-04 13:28:03 +0300

    clear old version from main

--- backend/mqtt/src/main/java/pro/ivanov/mqtt/MyGateway.java
+++
@@ -1,9 +0,0 @@
-package pro.ivanov.mqtt;
-
-import org.springframework.integration.annotation.MessagingGateway;
-
-@MessagingGateway(defaultRequestChannel="mqttChannel")
-public interface MyGateway {
-
-    void sendToMqtt(String data);
-}
