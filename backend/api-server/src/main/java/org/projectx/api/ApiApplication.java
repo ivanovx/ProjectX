d7a0d1bbb5000d98ab9f8f297e5c8bf5ec337884
Ivan Ivanov <me@ivanov.pro>
2023-09-08 11:02:46 +0300

    basic pom updates

--- backend/api-server/src/main/java/org/projectx/api/ApiApplication.java
+++ backend/api-server/src/main/java/org/projectx/api/ApiApplication.java
@@ -2,7 +2,6 @@ package org.projectx.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 public class ApiApplication {
