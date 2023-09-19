8d58240478b04e055a87f1efca81d8c854a0f32d
Ivan Ivanov <ivancivanovbg@gmail.com>
2023-09-19 12:47:54 +0300

    update common settings

--- backend/device-service/src/main/java/org/projectx/device/DeviceController.java
+++ backend/device-service/src/main/java/org/projectx/device/DeviceController.java
@@ -1,17 +1,10 @@
 package org.projectx.device;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/devices")
 public class DeviceController {
-    @GetMapping
-    public ResponseEntity all(Principal principal) {
-        return ResponseEntity.status(200).body(principal);
-    }
+
 }
