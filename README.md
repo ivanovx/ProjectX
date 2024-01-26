9d870feaa41bbab00dc920c1c6073982b59813ed
Ivan Ivanov <me@ivanov.pro>
2024-01-26 18:17:41 +0200

    base idea for cors

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Sensor Network
 
 Make open weather data from community sensors.
+
+## Architecture
+
+* auth-service
+
+* device-service
+* measurement-service
+* token-service
+* stat-service
+
+* gateway-service
+* discovery-service
--- backend/gateway-service/src/main/java/org/sensornetwork/gateway/GatewayApplication.java
+++ backend/gateway-service/src/main/java/org/sensornetwork/gateway/GatewayApplication.java
@@ -4,6 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/*
+
+spring:
+  cloud:
+    gateway:
+      globalcors:
+        cors-configurations:
+          '[/**]':
+            allowedOrigins: "https://docs.spring.io"
+            allowedMethods:
+            - GET
+
+ */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
