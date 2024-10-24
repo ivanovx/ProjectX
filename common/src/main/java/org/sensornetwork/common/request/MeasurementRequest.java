51967a7ccecc88f704b6cff092f69563bfd2a8c8
Ivan Ivanov <me@ivanov.pro>
2024-10-24 17:12:46 +0300

    dockersize frontend part

--- common/src/main/java/org/sensornetwork/common/request/MeasurementRequest.java
+++ common/src/main/java/org/sensornetwork/common/request/MeasurementRequest.java
@@ -3,8 +3,8 @@ package org.sensornetwork.common.request;
 import java.util.Optional;
 
 public record MeasurementRequest(
-         double temperature,
-         double uvIndex,
-         double vocIndex,
-         double airQualityIndex
+    double temperature,
+    double uvIndex,
+    double vocIndex,
+    double airQualityIndex
 ) { }
